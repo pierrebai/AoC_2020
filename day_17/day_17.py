ec3b193286859d7c07311504abcdbbff81a651de
Pierre Baillargeon <pierrebai@hotmail.com>
2020-12-17 15:25:46 -0500

    day 17

--- day_17/day_17.py
+++ day_17/day_17.py
@@ -1,3 +1,37 @@
+input_data = list(filter(None, open('day_17/input.txt').read().split('\n')))
+cubes = {}
+for y, line in enumerate(input_data):
+    for x, c in enumerate(line):
+        if c == '#':
+            cubes[(x, y, 0)] = 1
 
-input_data = filter(None, open('day_17/input.txt').read().split('\n'))
+def count_neighbours(cubes, x: int, y: int, z: int):
+    count = 0
+    for dx in range(-1, 2):
+        for dy in range(-1, 2):
+            for dz in range(-1, 2):
+                if dx != 0 or dy != 0 or dz != 0:
+                    if (x+dx, y+dy, z+dz) in cubes:
+                        count += 1
+    return count
 
+def execute_cycle(cubes):
+    new_cubes = {}
+    done = set()
+    for x, y, z in cubes.keys():
+        for dx in range(-1, 2):
+            for dy in range(-1, 2):
+                for dz in range(-1, 2):
+                    coord = (x+dx, y+dy, z+dz)
+                    if coord not in done:
+                        done.add(coord)
+                        active = coord in cubes
+                        count = count_neighbours(cubes, *coord)
+                        if count == 3 or (count == 2 and active):
+                            new_cubes[coord] = 1
+    return new_cubes
+
+for i in range(0,6):
+    cubes = execute_cycle(cubes)
+
+print(str(len(cubes)))
