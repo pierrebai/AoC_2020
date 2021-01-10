e7f61a663ccbb981f6bb55b090cdd2623fe4d74c
Pierre Baillargeon <pierrebai@hotmail.com>
2021-01-09 20:18:05 -0500

    Optimized day 15 to not use constantly recreated array.

--- day_15/day_15.py
+++ day_15/day_15.py
@@ -2,32 +2,31 @@ def input():
     return [1,12,0,20,8,16]
     #return [0,3,6]
 
-def play_until(spoken_when, just_spoken, current_turn, until_turn):
-    while current_turn < until_turn:
-        if len(spoken_when[just_spoken]) > 1:
-            age = spoken_when[just_spoken][-1] - spoken_when[just_spoken][-2]
+def play_until(spoken_when, spoken_before, just_spoken, current_turn, until_turn):
+    for current_turn in range(current_turn, until_turn):
+        if just_spoken in spoken_before:
+            just_spoken = spoken_when[just_spoken] - spoken_before[just_spoken]
         else:
-            age = 0
+            just_spoken = 0
 
         #print(age)
 
-        if age in spoken_when:
-            spoken_when[age] = [spoken_when[age][-1], current_turn]
+        if just_spoken in spoken_when:
+            spoken_before[just_spoken] = spoken_when[just_spoken]
+            spoken_when[just_spoken] = current_turn
         else:
-            spoken_when[age] = [current_turn]
-
-        just_spoken = age
-        current_turn += 1
+            spoken_before[just_spoken] = current_turn
+            spoken_when[just_spoken] = current_turn
 
     return just_spoken
 
 def part_1(input_data):
-    spoken_when = { k:[v] for v,k in enumerate(input_data) }
-    return play_until(spoken_when, input_data[-1], len(input_data), 2020)
+    spoken_when = { k:v for v,k in enumerate(input_data) }
+    return play_until(spoken_when, {}, input_data[-1], len(input_data), 2020)
 
 def part_2(input_data):
-    spoken_when = { k:[v] for v,k in enumerate(input_data) }
-    return play_until(spoken_when, input_data[-1], len(input_data), 30000000)
+    spoken_when = { k:v for v,k in enumerate(input_data) }
+    return play_until(spoken_when, {}, input_data[-1], len(input_data), 30000000)
 
 if __name__ == '__main__':
     print(part_1(input()))
