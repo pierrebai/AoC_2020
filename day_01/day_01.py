39d025c94c91506265d70176b62c016365dd9407
Pierre Baillargeon <pierrebai@hotmail.com>
2021-01-10 13:08:46 -0500

    Converted all solution to Jupyter lab notebook format.

--- day_01/day_01.py
+++ day_01/day_01.py
@@ -1,8 +1,8 @@
-def clean_lines(file_name):
-    return filter(None, map(lambda l2: l2.strip(), open(file_name)))
-
 def input():
     return set(map(lambda x: int(x), clean_lines('day_01/input.txt')))
+
+def clean_lines(file_name):
+    return filter(None, map(lambda l2: l2.strip(), open(file_name)))
 
 def match_target(values, target):
     for v in values:
@@ -11,20 +11,14 @@ def match_target(values, target):
             return v * complement
     return None
 
-def match_2020(values):
+def part_1(values):
     return match_target(values, 2020)
 
-def match_3_2020(values):
+def part_2(values):
     for v in values:
         matches = match_target(values, 2020 - v)
         if matches:
             return v * matches
-
-def part_1(input_data):
-    return match_2020(input_data)
-
-def part_2(input_data):
-    return match_3_2020(input_data)
 
 if __name__ == '__main__':
     print(part_1(input()))
